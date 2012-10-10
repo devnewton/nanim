fddc9a86d7dcf7ac062a8ade74c522f766cd74a1
devnewton <devnewton>
2012-10-10 22:10:58 +0000

    add combo box to choose animation to play in nanim view

--- nanimview/src/main/java/im/bci/NanimView.java
+++ nanimview/src/main/java/im/bci/NanimView.java
@@ -38,19 +38,25 @@ import im.bci.nanim.NanimParser.Frame;
 import im.bci.nanim.NanimParser.Image;
 import im.bci.nanim.NanimParser.Nanim;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Vector;
 
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -70,61 +76,58 @@ public class NanimView extends JFrame {
 	private CommandLine commandLine;
 	private Nanim nanim;
 	private HashMap<String, BufferedImage> loadedBufferedImages = new HashMap<String, BufferedImage>();
+	private NanimViewPanel viewPanel;
 
 	class NanimViewPanel extends JPanel implements ActionListener {
 
 		private static final long serialVersionUID = 1248894247319903536L;
-		int currentAnimation = 0;
-		int currentFrame = 0;
+		Animation currentAnimation;
+		Frame currentFrame;
+		int currentFrameIndex;
 
 		public NanimViewPanel() {
 			setDoubleBuffered(true);
-			updateAnimation();
 			setPreferredSize(computeMaxFrameSize());
+			setAlignmentX(Component.CENTER_ALIGNMENT);
 		}
 
 		private void updateAnimation() {
-			++currentFrame;
-			Animation animation = nanim.getAnimations(currentAnimation);
-			if (currentFrame >= animation.getFramesCount()) {
-				currentFrame = 0;
-				++currentAnimation;
-				if (currentAnimation >= nanim.getAnimationsCount()) {
-					currentAnimation = 0;
-					animation = nanim.getAnimations(currentAnimation);
+			if (null != currentAnimation) {
+				++currentFrameIndex;
+
+				if (currentFrameIndex >= currentAnimation.getFramesCount()) {
+					currentFrameIndex = 0;
 				}
+				currentFrame = currentAnimation.getFrames(currentFrameIndex);
+				Timer timer = new Timer(currentFrame.getDuration(), this);
+				timer.setRepeats(false);
+				timer.start();
 			}
-			Frame frame = animation.getFrames(currentFrame);
-			Timer timer = new Timer(frame.getDuration(), this);
-			timer.setRepeats(false);
-			timer.start();
 		}
 
 		@Override
 		public void paint(Graphics g) {
-			Frame frame = nanim.getAnimations(currentAnimation).getFrames(
-					currentFrame);
-			// Image image = nanim.getImages()
-			BufferedImage loadedImage = loadedBufferedImages.get(frame
-					.getImageName());
-			Image image = findImageByName(frame.getImageName());
-			Graphics2D g2d = (Graphics2D) g;
-			g2d.clearRect(0, 0, this.getWidth(), this.getHeight());
-			int sw = image.getWidth();
-			int sh = image.getHeight();
-			int w = Math.abs((int)(sw * (frame.getU2() - frame.getU1())));
-			int h = Math.abs((int)(sh * (frame.getV2() - frame.getV1())));
-			int sx1 = (int) (sw * frame.getU1());
-			int sy1 = (int) (sh * frame.getV1());
-			int sx2 = (int) (sw * frame.getU2());
-			int sy2 = (int) (sh * frame.getV2());
-			g2d.drawImage(loadedImage, 0, 0, w,
-					h,
-					sx1,
-					sy1,
-					sx2,
-					sy2,
-					(ImageObserver) null);
+			if (null != currentFrame) {
+				BufferedImage loadedImage = loadedBufferedImages
+						.get(currentFrame.getImageName());
+				Image image = findImageByName(currentFrame.getImageName());
+				Graphics2D g2d = (Graphics2D) g;
+				g2d.clearRect(0, 0, this.getWidth(), this.getHeight());
+				int sw = image.getWidth();
+				int sh = image.getHeight();
+				int w = Math
+						.abs((int) (sw * (currentFrame.getU2() - currentFrame
+								.getU1())));
+				int h = Math
+						.abs((int) (sh * (currentFrame.getV2() - currentFrame
+								.getV1())));
+				int sx1 = (int) (sw * currentFrame.getU1());
+				int sy1 = (int) (sh * currentFrame.getV1());
+				int sx2 = (int) (sw * currentFrame.getU2());
+				int sy2 = (int) (sh * currentFrame.getV2());
+				g2d.drawImage(loadedImage, 0, 0, w, h, sx1, sy1, sx2, sy2,
+						(ImageObserver) null);
+			}
 		}
 
 		public void actionPerformed(ActionEvent e) {
@@ -132,18 +135,28 @@ public class NanimView extends JFrame {
 			repaint();
 		}
 
+		public void setCurrentAnimation(String name) {
+			boolean notStartedYet = null == currentAnimation;
+			currentAnimation = findAnimationByName(name);
+			if (notStartedYet) {
+				updateAnimation();
+			}
+		}
 	}
 
 	public NanimView(CommandLine line) throws IOException {
 		this.commandLine = line;
 		decode();
 		load();
-		
+
 		setMinimumSize(new Dimension(128, 128));
 
-		GridBagLayout layout = new GridBagLayout();
+		BoxLayout layout = new BoxLayout(this.getContentPane(),
+				BoxLayout.Y_AXIS);
 		this.getContentPane().setLayout(layout);
-		this.getContentPane().add(new NanimViewPanel());
+		viewPanel = new NanimViewPanel();
+		this.getContentPane().add(createAnimationsComboBox());
+		this.getContentPane().add(viewPanel);
 		pack();
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -152,22 +165,56 @@ public class NanimView extends JFrame {
 		setVisible(true);
 	}
 
+	private Component createAnimationsComboBox() {
+		Vector<String> animationNames = new Vector<String>();
+		for (Animation animation : nanim.getAnimationsList()) {
+			animationNames.add(animation.getName());
+		}
+		Collections.sort(animationNames);
+		viewPanel.setCurrentAnimation(animationNames.get(0));
+		JComboBox<String> combo = new JComboBox<String>(animationNames);
+		combo.setAlignmentX(Component.CENTER_ALIGNMENT);
+		combo.addItemListener(new ItemListener() {
+
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					viewPanel.setCurrentAnimation((String) e.getItem());
+				}
+			}
+		});
+		return combo;
+	}
+
+	protected Animation findAnimationByName(String name) {
+		for (Animation animation : nanim.getAnimationsList()) {
+			if (animation.getName().equals(name)) {
+				return animation;
+			}
+		}
+		return null;
+	}
+
 	private Dimension computeMaxFrameSize() {
 		int w = 16, h = 16;
-		for(Animation animation : nanim.getAnimationsList()){
-			for(Frame frame : animation.getFramesList()) {
+		for (Animation animation : nanim.getAnimationsList()) {
+			for (Frame frame : animation.getFramesList()) {
 				Image image = findImageByName(frame.getImageName());
-				int frameW = Math.abs((int)(image.getWidth() * (frame.getU2() - frame.getU1())));
-				int frameH = Math.abs((int)(image.getHeight() * (frame.getV2() - frame.getV1())));
-				if(frameW>w)
+				int frameW = Math
+						.abs((int) (image.getWidth() * (frame.getU2() - frame
+								.getU1())));
+				int frameH = Math
+						.abs((int) (image.getHeight() * (frame.getV2() - frame
+								.getV1())));
+				if (frameW > w)
 					w = frameW;
-				if(frameH>h)
+				if (frameH > h)
 					h = frameH;
 			}
 		}
 		return new Dimension(w, h);
 	}
-	
+
 	private Image findImageByName(String imageName) {
 		for (Image image : nanim.getImagesList()) {
 			if (image.getName().equals(imageName))
@@ -188,8 +235,8 @@ public class NanimView extends JFrame {
 
 		GnuParser parser = new GnuParser();
 		CommandLine line = parser.parse(options, args);
-		
-		if(!line.hasOption("i")) {
+
+		if (!line.hasOption("i")) {
 			HelpFormatter formatter = new HelpFormatter();
 			formatter.printHelp("nanimenc [args]", options);
 			return;
